87ccdc12b1ab8e17c4dc8bdb9a0f62d7c87ec6a2
unknown <javierhack>
2021-01-25 07:58:28 -0500

    Versión 1

---
+++ DownloadTube.py
@@ -0,0 +1,27 @@
+
+#DESCARGA VIDEOS DE YOUTUBE
+
+from pytube import YouTube
+print("------------------------------------")
+print(" DESCARGA VIDEOS DE YOUTUBE")
+print("------------------------------------" +"\n")
+
+
+
+usuario= input("Ingrese la URL: ")
+print("-------------------------------------")
+print(" OBTENIENDO INFORMACIÓN DEL VIDEO")
+print("-------------------------------------"+"\n")
+dsc = YouTube(usuario)
+
+
+
+print("-------------------------------------")
+print(" ESPERE MIENTRAS SE DESCARGA")
+print("-------------------------------------")
+
+dsc.streams.first().download(r"/data/data/com.termux/files/home/storage/downloads")
+
+#dsc.streams.first().download()
+
+#JavierHack
